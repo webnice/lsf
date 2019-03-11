b79380060d92472229368140a1baffe747466290
Alex Geer <monoflash@gmail.com>
2019-03-12 02:59:30 +0300

    Added test

--- lsf/lsf_test.go
+++ lsf/lsf_test.go
@@ -3,6 +3,7 @@ package lsf
 //import "gopkg.in/webnice/debug.v1"
 //import "gopkg.in/webnice/log.v2"
 import (
+	"net/url"
 	"testing"
 )
 
@@ -25,6 +26,38 @@ func TestNew(t *testing.T) {
 	}
 	if qfo.order == nil {
 		t.Fatalf("New not created oreder slice")
+		return
+	}
+}
+
+func TestBuild(t *testing.T) {
+	const (
+		baseURL     = `https://example.com/api/v1.0/administration/action/method`
+		expectation = `https://example.com/api/v1.0/administration/action/method?filter=id%3Age%3A1&filter=name%3Ane%3A&filter=name%3Ake%3A%2Aion&limit=0%3A2&order=updateAt%3Adesc&order=createAt%3Aasc&order=name%3Aasc&order=id%3Aasc`
+	)
+	var err error
+	var uri *url.URL
+	var qfo Interface
+
+	if uri, err = url.Parse(baseURL); err != nil {
+		t.Fatalf("net/url.Parse(%q) error: %s", baseURL, err)
+		return
+	}
+	qfo = New().
+		Limit(0, 2).
+		Order(`updateAt`, SortDescending).
+		Order(`createAt`, SortAscending).
+		Order(`name`, SortAscending).
+		Order(`id`, SortAscending).
+		Filter(`id`, EqGreaterEqual, "1").
+		Filter(`name`, EqNotEqual, "").
+		Filter(`name`, EqLikeThan, "*ion")
+	if uri, err = qfo.URL(uri); err != nil {
+		t.Fatalf("create URL error: %s", err)
+		return
+	}
+	if uri.String() != expectation {
+		t.Fatalf("expected %q, return %q", expectation, uri.String())
 		return
 	}
 }
